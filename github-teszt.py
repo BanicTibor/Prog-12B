3f24835459640121f516509278d2ffc50c2e5dc1
Banic Tibor <banictibi@gmail.com>
2026-09-17 08:42:40 +0200

    Add even number check

--- github-teszt.py
+++ github-teszt.py
@@ -5,3 +5,10 @@ osszeg=0
 for szam in szamok:
     osszeg+=szam
 print(f"Átlag: {osszeg/len(szamok)}")
+index=0
+while i < len(szamok) and szamok[index]%2 != 0:
+    index+=1
+if index<len(szamok):
+    print("Van páros szám!")
+else:
+    print("Nincs páros szám!")
